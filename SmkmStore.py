a3fb6def864b6a325d05c32b2b73022975f9e9fd
cc <cc@cc.cc>
2019-07-20 17:48:19 -0400

    SMKM_STORE working on 1703.x86 & x64

--- SmkmStore.py
+++ SmkmStore.py
@@ -11,27 +11,29 @@ class SmkmStore(RamPack):
         self.fe = self.get_flare_emu()
         return
 
-    def _dump32(self):
-        self.logger.info("SMKM_STORE.StStore: {0:#x}".format(self.Info.arch_fns['x86']['sks32_ststore'](self)))
-        self.logger.info("SMKM_STORE.pCompressedRegionPtrArray: {0:#x}".format(self.Info.arch_fns['x86']['sks32_compressedregionptrarray'](self)))
-        self.logger.info("SMKM_STORE.StoreOwnerProcess: {0:#x}".format(self.Info.arch_fns['x86']['sks32_storeownerprocess'](self)))
+    def _dump(self):
+        arch = 'x64' if self.Info.is_64bit() else 'x86'
+        self.logger.info("SMKM_STORE.StStore: {0:#x}".format(self.Info.arch_fns[arch]['sks_ststore'](self)))
+        self.logger.info("SMKM_STORE.pCompressedRegionPtrArray: {0:#x}".format(self.Info.arch_fns[arch]['sks_compressedregionptrarray'](self)))
+        self.logger.info("SMKM_STORE.StoreOwnerProcess: {0:#x}".format(self.Info.arch_fns[arch]['sks_storeownerprocess'](self)))
         return
 
     def _dump64(self):
         return
 
     @RamPack.Info.arch32
-    def sks32_ststore(self):
+    @RamPack.Info.arch64
+    def sks_ststore(self):
         # Assumption is that the ST_STORE struct is nested @ offset 0
         return 0
 
     @RamPack.Info.arch32
-    def sks32_compressedregionptrarray(self):
+    @RamPack.Info.arch64
+    def sks_compressedregionptrarray(self):
         (fn_addr, fn_name) = self.find_ida_name("SmStMapVirtualRegion")
         pat = self.patgen(8192)
         lp_smkmstore = self.fe.loadBytes(pat)
-        regState = {'ecx': lp_smkmstore}
-
+        reg_cx = 'rcx' if self.Info.is_64bit() else 'ecx'
         mHookData = {'offset': None, 'structAddr': lp_smkmstore}
 
         def mHook(uc, accessType, memAccessAddress, memAccessSize, memValue, userData):
@@ -48,19 +50,23 @@ class SmkmStore(RamPack):
             else:
                 self.logger.error("Mem unknown accessType @ 0x{0:x}".format(memAccessAddress))
 
-        mu = self.fe.emulateRange(fn_addr, registers=regState, instructionHook=self.eHookTrace, memAccessHook=mHook)
+        regState = {reg_cx: lp_smkmstore}
+        self.fe.emulateRange(fn_addr, registers=regState, memAccessHook=mHook)
         return mHookData['offset'] - mHookData['structAddr']
 
     @RamPack.Info.arch32
-    def sks32_storeownerprocess(self):
+    @RamPack.Info.arch64
+    def sks_storeownerprocess(self):
         (startAddr, endAddr) = self.locate_call_in_fn("?SmStDirectRead@?$SMKM_STORE", "KiStackAttachProcess")
         pat = self.patgen(8192)
         addr_smkmstore = self.fe.loadBytes(pat)
+        reg_cx = 'rcx' if self.Info.is_64bit() else 'ecx'
+        struct_fmt = "<Q" if self.Info.is_64bit() else "<I"
 
         def pHook(self, userData, funcStart):
             self.logger.debug("pre emulation hook loading ECX")
             userData["EmuHelper"].uc.reg_write(userData["EmuHelper"].regs["cx"], addr_smkmstore)
 
         self.fe.iterate([endAddr], self.tHook, preEmuCallback=pHook)
-        reg_ecx = self.fe.getRegVal("ecx")
-        return pat.find(struct.pack("<I", reg_ecx))
+        reg_ecx = self.fe.getRegVal(reg_cx)
+        return pat.find(struct.pack(struct_fmt, reg_ecx))
