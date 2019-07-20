440cf66600f7825f763f5e7022b46daaa1f57d50
cc <cc@cc.cc>
2019-07-20 14:20:30 -0400

    SMKM_STORE_MGR working on 1703 x86 & x64

--- SmkmStoreMgr.py
+++ SmkmStoreMgr.py
@@ -9,22 +9,30 @@ class SmkmStoreMgr(RamPack):
         self.fe = self.get_flare_emu()
         return
 
-    def _dump32(self):
-        self.logger.info("SMKM_STORE_MGR.Smkm: 0x{0:x}".format(self.Info.arch_fns['x86']['sksm32_smkm'](self)))
-        self.logger.info("SMKM_STORE_MGR.BTreeGlobalStore: 0x{0:x}".format(self.Info.arch_fns['x86']['sksm32_globaltree'](self)))
-        return
-
-    def _dump64(self):
+    def _dump(self):
+        if self.Info.is_64bit():
+            self.logger.info("SMKM_STORE_MGR.sSmKm: {0:#x}".format(self.Info.arch_fns['x64']['sksm_smkm'](self)))
+            self.logger.info("SMKM_STORE_MGR.sGlobalTree: {0:#x}".format(self.Info.arch_fns['x64']['sksm_globaltree'](self)))
+        else:
+            self.logger.info("SMKM_STORE_MGR.sSmKm: {0:#x}".format(self.Info.arch_fns['x86']['sksm_smkm'](self)))
+            self.logger.info("SMKM_STORE_MGR.sGlobalTree: {0:#x}".format(self.Info.arch_fns['x86']['sksm_globaltree'](self)))
         return
 
     @RamPack.Info.arch32
-    def sksm32_smkm(self):
+    @RamPack.Info.arch64
+    def sksm_smkm(self):
         return 0  # constant across win10
 
     @RamPack.Info.arch32
-    def sksm32_globaltree(self):
+    @RamPack.Info.arch64
+    def sksm_globaltree(self):
         (startAddr, endAddr) = self.locate_call_in_fn("?SmFeCheckPresent",
                                                       "?BTreeSearchKey@?$B_TREE@T_SM_PAGE_KEY@@USMKM_FRONTEND_ENTRY")
+        if self.Info.is_64bit():
+            reg_cx = 'rcx'
+        else:
+            reg_cx = 'ecx'
+
         # @start ecx is SmkmStoreMgr and instantiated to 0.
         # @end ecx is the pushlock argument, diff to get struct offset
         addr_smkmstoremgr = 0x1000
@@ -34,4 +42,4 @@ class SmkmStoreMgr(RamPack):
             userData["EmuHelper"].uc.reg_write(userData["EmuHelper"].regs["cx"], addr_smkmstoremgr)
 
         self.fe.iterate([endAddr], self.tHook, preEmuCallback=pHook)
-        return self.fe.getRegVal('ecx') - addr_smkmstoremgr
+        return self.fe.getRegVal(reg_cx) - addr_smkmstoremgr
