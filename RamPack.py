ca42668862c598fd06b45bf4b59adb7449f60602
cc <cc@cc.cc>
2019-07-20 14:19:36 -0400

    Removed capstone by modifying iter_fn. Changed return value of locate_call_in_fn to be more accurate & support iterate.

--- RamPack.py
+++ RamPack.py
@@ -5,8 +5,6 @@ import idc
 import idautils
 import idaapi
 
-import capstone
-
 from flare_emu import flare_emu
 
 
@@ -42,21 +40,16 @@ class RamPack():
                 self.logger.debug("found {0} @ {1}".format(name_addr[1], hex(name_addr[0])))
                 return name_addr
         self.logger.error("{0} NOT FOUND".format(fn_name))
-
-    def get_cs(self):
-        cs = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
-        cs.detail = True
-        return cs
+        return None
 
     def get_flare_emu(self, loglevel=logging.INFO):
         fe = flare_emu.EmuHelper(loglevel=loglevel)
         return fe
 
-    def iter_fn(self, addr):
-        cs = self.get_cs()
-        for insn_addr in idautils.FuncItems(addr):
-            for insn in cs.disasm(idc.GetManyBytes(insn_addr, idc.ItemSize(insn_addr)), insn_addr):
-                yield insn
+    def iter_fn(self, startAddr):
+        endAddr = idc.GetFunctionAttr(startAddr, idc.FUNCATTR_END)
+        for head in idautils.Heads(startAddr, endAddr):
+            yield head, idc.GetDisasm(head).split()[0], idc.GetOpnd(head, 0), idc.GetOpnd(head, 1)
 
     def locate_call_in_fn(self, fns_start, fns_call):
         if type(fns_start) is not list:
@@ -65,22 +58,20 @@ class RamPack():
             fns_call = [fns_call]
 
         for fn_start in fns_start:
+            (addr_start, name_start) = self.find_ida_name(fn_start)
             for fn_call in fns_call:
-                (addr_start, name_start) = self.find_ida_name(fn_start)
                 (addr_end, name_end) = self.find_ida_name(fn_call)
 
-                for insn in self.iter_fn(addr_start):
-                    if insn.id == capstone.x86.X86_INS_CALL:
-                        if insn.operands[0].type == capstone.x86.X86_OP_IMM:
-                            call_offset = insn.operands[0].imm
-                            if call_offset == addr_end:
-                                addr_end = insn.address
-                                self.logger.debug("located {0} in {1} @ 0x{2:x}".format(name_end, name_start, addr_end))
-                                return (addr_start, addr_end)
-                self.logger.error("Failed to locate {0} within {1}".format(name_end, name_start))
+                for insn_addr, insn, op0, op1 in self.iter_fn(addr_start):
+                    if insn == "call":
+                        if op0 == name_end:
+                            self.logger.debug("located {0} in {1} @ {2:#x}".format(name_end, name_start, addr_end))
+                            return addr_start, insn_addr
+
+                self.logger.debug("Failed to locate {0} within {1}".format(name_end, name_start))
 
         self.logger.error("locate_call_in_fn failed")
-        return (None, None)
+        return None, None
 
     @staticmethod
     def patgen(buf_len, size=4):
@@ -113,13 +104,9 @@ class RamPack():
                     if buf_len < 0:
                         return pat
 
-        else:
-            self.logger.error("Unsupported size fed to pattern generator")
-
     """
     Use with instructionHook callback to get a disassembly + reg dump
     """
-
     def eHookDbg(self, uc, address, size, user_data):
         fe = user_data['EmuHelper']
         dis = idc.GetDisasm(address)
